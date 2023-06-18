5a295c50b6698806dc4e43aa658d218e054c09e2
Denis Dinkov <denisdinkov7@gmail.com>
2023-06-18 18:42:21 +0300

    .

--- src/Views/LearningView.vue
+++ src/Views/LearningView.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <h1>Course Collection</h1>
+    <TheHeader></TheHeader>
     <CollectionTable></CollectionTable>
     <TheFooter></TheFooter>
   </div>
 </template>
 
 <script>
+  import TheHeader from '../components/TheHeader.vue'
   import TheFooter from '../components/TheFooter.vue';
   import CollectionTable from '../components/CollectionTable.vue'
 export default{
   components: {
     TheFooter,
     CollectionTable,
+    TheHeader,
   }
 }</script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="header">
+    <span>Welcome student!</span>
+    <img src="../Assets/student.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+};
+</script>
+
+<style>
+.header {
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #2F5760;
+  top: 0;
+  z-index: 100;
+}
+.header > span {
+  padding: 25px;
+  color: #459fec;
+  font-size: 25px;
+}
+.header > span:hover {
+  color: #f1faf5;
+}
+.header > img {
+  width: 65px;
+  object-fit: contain;
+  margin: 20px 20px;
+  margin-top: 18px;
+  opacity: 1;
+}
+.header > img:hover {
+  opacity: 0.5;
+}
+</style>
